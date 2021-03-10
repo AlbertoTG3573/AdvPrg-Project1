7d50710af934f6f9679e5b7e3077a4ffd9b3d999
AlbertoTG3573 <78233820+AlbertoTG3573@users.noreply.github.com>
2021-03-10 18:52:43 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,106 @@
 #include <iostream>
 #include "PGM.h"
-using namespace std;
+#include "ImageProcessing.h"
 
+using namespace std;
+// Group: Alberto & Emma
 int main()
 {
     // Path to the file
     // If image is in the same directory as executable
     //string fileName = "lena.pgm";
+    bool cont;
+    char yesno = 'n';
+    const unsigned int COPY = 0;
+    const unsigned int FLIPVERT = 1;
+    const unsigned int FLIPHOR = 2;
+    const unsigned int MEDIAN = 3;
+    unsigned int choice = 0;
+    string fileInput;
+    string fileName;
+    string outputName;
 
-    // Clion paths if cloned from repo
-    string fileName = "..\\test_images\\lena.pgm";
-    string outputName = "..\\processed_images\\lena_copy.pgm";
+    do {
+        cout << "Enter Original File Name: ";
+        cin >> fileInput;
+        fileName = "..\\test_images\\" + fileInput;
+        //Open File set information
+        if (openPGM(fileName)) {
+            // Get Image Size Information
+            cout << "\nFile Successfully Opened!" << endl;
+            int width = getPGMWidth();
+            int height = getPGMHeight();
 
-    //Open File set information
-    if( openPGM(fileName) )
-    {
-        // Get Image Size Information
-        int width = getPGMWidth();
-        int height = getPGMHeight();
+            // Declare and allocate memory for data
+            int **original;
+            original = new int *[height];
 
-        // Declare and allocate memory for data
-        int** original; 	        
-        original = new int*[ height];          
-        
-        for(int i = 0; i < height; i++){
-            original[i] = new int[width];  		             
+            for (int i = 0; i < height; i++) {
+                original[i] = new int[width];
+            }
+
+            // Get the data
+            getPGMData(original);
+
+            // Declare and allocate memory for copy data
+            int **copy;
+            copy = new int *[height];
+
+            for (int i = 0; i < height; i++) {
+                copy[i] = new int[width];
+            }
+            //get operation int from console
+            cout << "[0] Copy Image\n[1] Flip Vertical\n[2] Flip Horizontal\n[3] Median Filter\nEnter Selection: ";
+            cin >> choice;
+            cout << "\nEnter Save File Name: ";
+            cin >> fileInput;
+            outputName = "..\\processed_images\\" + fileInput;
+            cout << "\nPreforming Operation..." << endl;
+
+            // choose operation based on choice
+            switch(choice){
+                case COPY:
+                    copyImage(original,width,height);
+                    break;
+                case FLIPVERT:
+                    flipVertical(original,width,height);
+                    break;
+                case FLIPHOR:
+                    flipHorizontal(original,width,height);
+                    break;
+                case MEDIAN:
+                    medianFilter(original,width,height);
+                    break;
+            }
+            cout << "Writing out the File...";
+
+            // Write back out the same image
+            if (!writePGM(outputName, original)) {
+                cout << "\nFailed to write out file" << endl;
+                cont = false;
+            }
+            cout << "Cleaning up now!\n";
+            // Clean up memory
+            for (int i = 0; i < height; i++) {
+                delete[] original[i];
+            }
+            delete original;
+            // prompt for continue
+            cout << "Clean-up finished.\nPreform another operation [y/n]? ";
+
+            cin >> yesno;
+            // check if yesno is equal to no
+            if ((yesno != 'n') && (yesno != 'N')){
+                cont = true;
+            }
+            else{
+                cont = false;
+            }
+
+        } else {
+            cout << "\nCannot open file " << fileName << endl;
+            cont = false;
         }
-        
-        // Get the data
-        getPGMData(original);	
-
-        // Write back out the same image
-        if( !writePGM(outputName, original) ){
-            cout << "Failed to write out file\n";
-        }
-
-        // Clean up memory
-        for(int i = 0; i < height; i++){
-            delete[] original[i];  	            
-        }	         
-        delete original;
-    }
-    else
-    {
-        cout << "Cannot open file " << fileName << endl;
-    }
+    }while(cont);
+    return 0;
 }
